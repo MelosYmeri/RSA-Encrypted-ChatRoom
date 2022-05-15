1debb5b23234c0ffad4c3bcafa9ef48f760f1370
melisaalaj <melisa.alaj.7@gmail.com>
2022-05-15 22:15:00 +0200

    Update client.py

--- client.py
+++ client.py
@@ -13,3 +13,16 @@ def assemble_pub_key_from_string(pub_key):
 	k = list(pub_key.decode('ISO-8859-1').split(','))
 	pub_key = PublicKey(int(k[0]),int(k[1]))
 	return pub_key
+
+QT_ENTER_KEY1 =  'special 16777220'
+QT_ENTER_KEY2 =  'special 16777221'
+
+
+name = input("Zgjidhni një emër: ")
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+def has_alpha(s):
+	for character in s:
+		if character.isalpha():
+			return True
+	return False
